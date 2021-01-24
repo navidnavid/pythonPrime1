26f25abbcb226c8c7ab821f178bc4df5d178966d
Navid Otadi <navid.otadi@gmail.com>
2021-01-24 18:08:01 +0100

    test git

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -4,53 +4,6 @@ import os
 
 def main():
 
-# read db file and change str to int 
-    cur_path = os.path.dirname(__file__)
-    new_path =  cur_path + '\\db\\db.txt' 
-    prm=[ 2, 3, 5]# for test 
-    file1 = open(new_path,"r") 
-    dbtxt= file1.read()
-    dbPrim= dbtxt.split(',')
-    prm= []
-    try:
-        for prNum in dbPrim:
-            prm.append(int( prNum ))
-    except:
-        prNum
-
-    #print(prm)
-
-# do algorithm
-    wn=10
-    wnComb = 2**wn
-    priObj = primMethod(0,[])
-    convRes = []
-    max_relation = []
-    maxP =0
-    for fn in range(wnComb) :
-        #convRes.append([])
-        temp = priObj.convfiltd(fn,prm)
-
-        #temp= priObj.deleteDub(temp)
-        foundPrim = priObj.matchQ(numpy.append(temp, prm))
-        if(foundPrim >0 ):
-            convRes.append([fn, foundPrim])# add prime then see matches
-            if(foundPrim> maxP):
-                max_relation = priObj.makeAry(fn)
-                maxP = foundPrim
-
-
-  
-        #convRes[n].append(temp)
-    meanOfMatches= numpy.mean(convRes)
-    print("---------max_relation------------------")
-    print(max_relation)
-    print("---------maxP found quantity-----------------")
-    print(maxP)
-    print("-----------meanOfMatches-------------")
-    print(meanOfMatches)
-
-    # an other goo didea is to make it mpdel based like logistic mapp to feet to it
 
 
 if __name__ == "__main__":
--- PythonApplication1/model/prim.py
+++ PythonApplication1/model/prim.py
@@ -2,48 +2,7 @@ import numpy
 
 class primMethod(object):
 
-    def __init__(self, fn, prmAry):
-            self.fn = fn
-            self.prmAry = prmAry
-
-    def makeAry(self,fn):
-        x=list(bin(fn).format(8))
-        del x[0]
-        del x[0]
-        return x
-            
-    def convfiltd(self,n , pAry):
-        x=list(bin(n).format(8))
-        del x[0]
-        del x[0]
-        # find signle 1 which means no linear relatoin
-
-        nState = list(map(int, x)) # converst to intiger
-        q_binaryAry = len(nState)-1
-        x =numpy.array(nState)
-        #ones_idx= numpy.where(x==1, )# remove single 1 relation
-        if ( sum(x) < 2): #remove single ones 
-            return 0
-        
-        res =( numpy.convolve(x, pAry))
-        res1 = res[q_binaryAry:] # remove first n elemnet
-        res2= res1[:len(res1)-q_binaryAry] #
-        return res2
-
-    def matchQ(self, resAry):
-        resAry.sort() 
-        resAryNp =numpy.array(resAry)
-        index = numpy.where((resAryNp == 0)) #find zeroe
-        resAryNp= numpy.delete(resAryNp, index) # delete zeros
-        resAryMOd= numpy.diff(resAryNp) # compare to nearest
-        pMatchFind_idx = numpy.where(resAryMOd == 0) # 0 = one found 
-        pMatchFindNpAry= numpy.array(pMatchFind_idx)
-        qPrimeFound = len(pMatchFindNpAry[0])
-        return qPrimeFound 
-        
-    def deleteDub(self,ary):
-        arySortd= numpy.unique(ary)
-        return arySortd
+   
 
 
     
